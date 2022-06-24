26a299fa496bfb388b208b70af07b7abf602b128
GonoBae <qldrhqorhsh@naver.com>
2022-06-24 22:50:21 +0900

    에셋 추가 및 Control 스크립트
    Script 는 만들기만 함

---
+++ ToDream/Assets/Scripts/Control/CharacterControlable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControlable : Controlable
+{
+	public override void Move(Vector2 input)
+	{
+		throw new System.NotImplementedException();
+	}
+	
+	public override void Rotate(Vector2 input)
+	{
+		throw new System.NotImplementedException();
+	}
+	
+	public override void JumpOrBreak()
+	{
+		throw new System.NotImplementedException();
+	}
+	
+	public override void Interact()
+	{
+		throw new System.NotImplementedException();
+	}
+}
---
+++ ToDream/Assets/Scripts/Control/Controller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+	private Controlable _controlTarget;
+	protected Controlable _ControlTarget
+	{
+		set { _controlTarget = value; } 
+		get { return _ControlTarget; } 
+	}
+}
