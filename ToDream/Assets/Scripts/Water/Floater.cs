c06cc1c5940e9e3214ae74332e772367b0620e4a
Sandor2889 <sandor2889@naver.com>
2022-06-26 02:28:33 +0900

    심플한 부력 구현 완료
    다음엔 Gerstner Wave와 비교해볼 필요 있음

---
+++ ToDream/Assets/Scripts/Water/Floater.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 객체의 rigid 설정 기준값  mass : 1, drag : 1
+public class Floater : MonoBehaviour
+{
+    public Rigidbody _rigid;
+    public float _depthBeforeSubmertged = 1f;   // 물에 잠기기전의 깊이
+    public float _displacementAmount = 3f;  //  변위량
+    //public float _floaterCount = 0;
+    public float _waterDrag = 0.99f; // 물 저항력
+    public float _waterAngularDrag = 0.5f; // 물 속 회전 저항력
+
+    private void Awake()
+    {
+        _rigid = GetComponentInParent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float waveHeight = WaveManager.GetWaveHeight(transform.position.x);
+        
+        if (transform.position.y < waveHeight)
+        {
+            // 부력
+            float displacementMultiplier = Mathf.Clamp01((waveHeight - transform.position.y) / _depthBeforeSubmertged) * _displacementAmount;
+            _rigid.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+            
+            // 물체가 물밖으로 튕기는 현상 제거
+            _rigid.AddForce(displacementMultiplier * -_rigid.velocity * _waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            
+            // 물체의 지나친 회전 억제
+            _rigid.AddTorque(displacementMultiplier * -_rigid.angularVelocity * _waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+    }
+}
