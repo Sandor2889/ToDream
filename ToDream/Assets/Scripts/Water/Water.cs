d1acdf2522f40172225ed036ede69d3d36010367
Sandor2889 <sandor2889@naver.com>
2022-07-04 21:17:42 +0900

    Change update method

--- ToDream/Assets/Scripts/Water/Water.cs
+++ ToDream/Assets/Scripts/Water/Water.cs
@@ -3,27 +3,34 @@ using System.Collections.Generic;
 using UnityEngine.Rendering;
 using UnityEngine;
 
+[ExecuteAlways]
 public class Water : MonoBehaviour
 {
     [SerializeField] private WaterResources _waterResources;
 
-
     private static readonly int _CameraRoll = Shader.PropertyToID("_CameraRoll");
     private static readonly int _InvViewProjection = Shader.PropertyToID("_InvViewProjection");
 
-    private void OnEnable()
+    [SerializeField] private Camera _cam;
+
+    private void Awake()
     {
-        Camera.onPreRender += MyPreRender;
-        Debug.Log("start");
+        _cam = Camera.main;
     }
-    
+
+    private void Update()
+    {
+
+        MyPreRender(_cam);
+    }
+
 
     private void MyPreRender(Camera cam)
     {
-        //var roll = cam.transform.localEulerAngles.z;
-        //Shader.SetGlobalFloat(_CameraRoll, roll);
-        //Shader.SetGlobalMatrix(_InvViewProjection,
-        //    (GL.GetGPUProjectionMatrix(cam.projectionMatrix, false) * cam.worldToCameraMatrix).inverse);
+        var roll = cam.transform.localEulerAngles.z;
+        Shader.SetGlobalFloat(_CameraRoll, roll);
+        Shader.SetGlobalMatrix(_InvViewProjection,
+            (GL.GetGPUProjectionMatrix(cam.projectionMatrix, false) * cam.worldToCameraMatrix).inverse);
 
         // Water matrix
         const float quantizeValue = 6.25f;
@@ -34,7 +41,6 @@ public class Water : MonoBehaviour
         newPos.y = yOffset;
         newPos.x = quantizeValue * (int)(newPos.x / quantizeValue);
         newPos.z = quantizeValue * (int)(newPos.z / quantizeValue);
-        Debug.Log(newPos.x + ", " + newPos.y + ", " + newPos.z);
 
         var matrix = Matrix4x4.TRS(newPos + transform.position, Quaternion.identity, transform.localScale); // transform.localToWorldMatrix;
 
@@ -55,9 +61,51 @@ public class Water : MonoBehaviour
         }
     }
 
-    private void OnDisable()
-    {
-        Camera.onPreRender -= MyPreRender;
-        Debug.Log("end");
-    }
+
+//    private void OnEnable()
+//    {
+//        Camera.onPreRender += MyPreRender;
+//    }
+
+
+//    private void MyPreRender(Camera cam)
+//    {
+//        var roll = cam.transform.localEulerAngles.z;
+//        Shader.SetGlobalFloat(_CameraRoll, roll);
+//        Shader.SetGlobalMatrix(_InvViewProjection,
+//            (GL.GetGPUProjectionMatrix(cam.projectionMatrix, false) * cam.worldToCameraMatrix).inverse);
+
+//        // Water matrix
+//        const float quantizeValue = 6.25f;
+//        const float forwards = 10f;
+//        const float yOffset = -0.25f;
+
+//        var newPos = cam.transform.TransformPoint(Vector3.forward * forwards);
+//        newPos.y = yOffset;
+//        newPos.x = quantizeValue * (int)(newPos.x / quantizeValue);
+//        newPos.z = quantizeValue * (int)(newPos.z / quantizeValue);
+
+//        var matrix = Matrix4x4.TRS(newPos + transform.position, Quaternion.identity, transform.localScale); // transform.localToWorldMatrix;
+
+//        foreach (var mesh in _waterResources.defaultWaterMeshes)
+//        {
+//            Graphics.DrawMesh(mesh,
+//                matrix,
+//                _waterResources.defaultSeaMaterial,
+//                gameObject.layer,
+//                cam,
+//                0,
+//                null,
+//                ShadowCastingMode.Off,
+//                true,
+//                null,
+//                LightProbeUsage.Off,
+//                null);
+//        }
+//    }
+
+//    private void OnDisable()
+//    {
+//        Camera.onPreRender -= MyPreRender;
+//    }
 }
--- ToDream/Assets/Scripts/Water/Water.shader
+++ ToDream/Assets/Scripts/Water/Water.shader
@@ -1,34 +1,29 @@
 Shader "Water"
 {
-    Properties
-    {
-        _BumpScale("Detail Wave Amount", Range(0, 2)) = 0.2 //fine detail multiplier
-        _DitherPattern("Dithering Pattern", 2D) = "bump" {}
-        [Toggle(_STATIC_SHADER)] _Static("Static", Float) = 0
-        [KeywordEnum(Off, SSS, Refraction, Reflection, Normal, Fresnel, WaterEffects, Foam, WaterDepth)] _Debug("Debug mode", Float) = 0
-    }
-    SubShader
-    {
-        Tags { "RenderType" = "Transparent" "Queue" = "Transparent-100"}
-        ZWrite On
-        
-        Pass
-        {
-            Name "WaterShading"
-            Tags{"LightMode" = "ForwardBase"}
+	Properties
+	{
+		_Color("Color", color) = (1, 1, 1, 1)
+	}
+	SubShader
+	{
+		Tags { "RenderType" = "Transparent" "Queue" = "Transparent-100" }
+		ZWrite On
 
-            HLSLPROGRAM
+		Pass
+		{
+			Name "WaterShading"
+			Tags{"LightMode" = "UniversalForward"}
 
-            ////////////////////INCLUDES//////////////////////
-            #include "WaterCommon.hlsl"
+			HLSLPROGRAM
+		////////////////////INCLUDES//////////////////////
+		//#include "WaterCommon.hlsl"
 
-            //non-tess
-            #pragma vertex WaterVertex
-            #pragma fragment WaterFragment
+		//non-tess
+		//#pragma vertex WaterVertex
+		//#pragma fragment WaterFragment
 
-            ENDHLSL
-        }
-        
-    }
-    FallBack "Diffuse"
+		ENDHLSL
+		}
+	}
+	FallBack "Diffuse"
 }
