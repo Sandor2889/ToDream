3bd3640fba9f306a51bca0813fb4a86c80f1ad92
GonoBae <qldrhqorhsh@naver.com>
2022-08-26 21:39:56 +0900

    Title Scene 추가

---
+++ ToDream/Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TitleManager : MonoBehaviour
+{
+	public GameObject _car;
+	
+	private void Start()
+    {
+	    Application.targetFrameRate = 60;
+    }
+    
+	private void Update()
+	{
+		if(!_car.activeSelf)
+		{
+			_car.SetActive(true);
+		}
+	}
+}
