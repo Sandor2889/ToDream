ca777cad09098beb8685fdd44d80f8a7db21016c
GonoBae <qldrhqorhsh@naver.com>
2022-08-31 21:47:56 +0900

    에디터 스크립트 수정

--- ToDream/Assets/Scripts/Flight/EditorFlightInput.cs
+++ ToDream/Assets/Scripts/Flight/EditorFlightInput.cs
@@ -1,33 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
 
-[CustomEditor(typeof(FlightInput))]
-public class EditorFlightInput : Editor
-{
-	private FlightInput _targetInput;
+//[CustomEditor(typeof(FlightInput))]
+//public class EditorFlightInput : Editor
+//{
+//	private FlightInput _targetInput;
 	
-	private void OnEnable()
-	{
-		_targetInput = target as FlightInput;
-	}
+//	private void OnEnable()
+//	{
+//		_targetInput = target as FlightInput;
+//	}
 	
-	public override void OnInspectorGUI()
-	{
-		//base.OnInspectorGUI();
+//	public override void OnInspectorGUI()
+//	{
+//		//base.OnInspectorGUI();
 		
-		//Custom editor changes
-		string debugInfo = "";
-		debugInfo += "Flight Input";
-		debugInfo += "\n\nSpeed : " + _targetInput._Speed;
-		debugInfo += "\nRoll : " + _targetInput._Roll;
-		debugInfo += "\nYaw : " + _targetInput._Yaw;
-		debugInfo += "\nPitch : " + _targetInput._Pitch;
+//		//Custom editor changes
+//		string debugInfo = "";
+//		debugInfo += "Flight Input";
+//		debugInfo += "\n\nSpeed : " + _targetInput._Speed;
+//		debugInfo += "\nRoll : " + _targetInput._Roll;
+//		debugInfo += "\nYaw : " + _targetInput._Yaw;
+//		debugInfo += "\nPitch : " + _targetInput._Pitch;
 
-		GUILayout.Space(20);
-		EditorGUILayout.TextArea(debugInfo, GUILayout.Height(100));
-		GUILayout.Space(20);
-		Repaint();
-	}
-}
+//		GUILayout.Space(20);
+//		EditorGUILayout.TextArea(debugInfo, GUILayout.Height(100));
+//		GUILayout.Space(20);
+//		Repaint();
+//	}
+//}
